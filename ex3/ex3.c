b2b8c09e36b758ff07a910a552c49d24739b070b
Basel <basel.obeid9661@gmail.com>
2018-10-29 22:39:47 +0100

    ex3

---
+++ ex3/ex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int age;
+    int height = 72;
+    int apa = 'a';
+    printf("aa %c \n", apa);
+    //printf("\xc0");
+    printf("I am %d years old. \n", age);
+    printf("I am %d inches tall. \n", height);    
+
+    return 0;
+}
