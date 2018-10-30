c7e8fd7c7379354187b00fdad555f4982eb0398d
Basel <basel.obeid9661@gmail.com>
2018-10-30 22:13:13 +0100

    ex4

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -9,6 +9,15 @@ int main(int argc, char const *argv[])
     //printf("\xc0");
     printf("I am %d years old. \n", age);
     printf("I am %d inches tall. \n", height);    
-
+    crash();
     return 0;
+}
+
+void crash() {
+    char *test = NULL;
+    int i = 0; 
+    for(i = 0; i < 1000; i++)
+    {
+        printf("%c", test[i]);
+    }
 }
